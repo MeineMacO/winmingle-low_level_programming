f1caec27fc18555f12b69000f7d1d5e6f79a0c9e
Meine Mac-Obegba <meinemaco@gmail.com>
2026-03-01 00:05:04 +0100

    A program that prints  all single-digit numbers of base 10 starting from 0 followed by a new line using only 2 putchars and int variable

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+*Authur: Meine Mac-Obegba
+*Program: WinMingle Community C Training
+*Description: Variables, If, Else, and While in C
+*Date: 28th February, 2026
+*/
+int main(void){
+    int x = 0;
+
+	while (x <= 9){
+	    putchar(48 + x);
+		x++;
+	}
+    putchar('\n');	
+    return(0);
+}
+
+
