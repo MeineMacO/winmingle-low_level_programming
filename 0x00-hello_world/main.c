cd3c2ca8e464b8513481fe89492637ee9b573acf
Meine Mac-Obegba <meinemaco@gmail.com>
2026-02-28 20:45:48 +0100

    A C file for the Preprocessor

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+*Authur: Meine Mac-Obegba
+*Program: WinMingle Community C Training
+*Description: Writing scipts that runs or compiles in C
+*/
+int main(void){
+
+    printf("We want to write a script that runs a C file through the preprocessor and saves the result into another file");
+    return(0);
+}
+
+
