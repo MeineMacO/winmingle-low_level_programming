9e29c2681eae96a09f8498e9005a6758f716c080
Meine Mac-Obegba <meinemaco@gmail.com>
2026-02-28 23:22:19 +0100

    A program that prints the alphabet in lowercase except letters q and e, followed by a new line using only 2 putchars.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+*Authur: Meine Mac-Obegba
+*Program: WinMingle Community C Training
+*Description: Variables, If, Else, and While in C
+*Date: 28th February, 2026
+*/
+int main(void){
+    char x = 'a';
+
+	while (x <= 'z'){
+	    if (x != 'e' && x != 'q'){
+	    	putchar(x);
+		}
+		x++;
+	}
+    putchar('\n');	
+    return(0);
+}
+
+
