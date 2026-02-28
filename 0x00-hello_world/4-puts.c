b02d67a1de2f9fe9a7d045d0a2f4c612a4e25ddf
Meine Mac-Obegba <meinemaco@gmail.com>
2026-02-28 20:31:18 +0100

    A C program that prints exactly this line, followed by a new line:

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+Authur: Meine Mac-Obegba
+Program: WinMingle Community C Training
+Description: Write a C program that prints exactly this line, followed by a new line:
+"Programming is like building a multilingual puzzle
+*/
+int main(void){
+    puts("\"Programming is like building a multilingual puzzle\n");
+    return(0);
+}
+
