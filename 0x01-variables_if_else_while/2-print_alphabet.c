cdc64b060f741ef5465f43fcf6494afb060ccf50
Meine Mac-Obegba <meinemaco@gmail.com>
2026-02-28 23:03:12 +0100

    A program that p rints the alphabet in lowercase, followed by a new line using only 2 putchars.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+*Authur: Meine Mac-Obegba
+*Program: WinMingle Community C Training
+*Description: Variables, If, Else, and While in C
+*Date: 28th February, 2026
+*/
+int main(void){
+    char x = 'a';
+
+	while (x <= 'z'){
+	    putchar(x);
+		x++;
+	}
+    putchar('\n');	
+    return(0);
+}
+
+
