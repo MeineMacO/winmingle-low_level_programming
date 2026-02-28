d5e3dcfdebf8fe96c678cc4f5c8c1c5433180832
Meine Mac-Obegba <meinemaco@gmail.com>
2026-02-28 20:33:15 +0100

    A C program that prints with printf, followed by a new line:

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+Authur: Meine Mac-Obegba
+Program: WinMingle Community C Training
+Description: Write a C program that prints with printf
+*/
+int main(void){
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return(0);
+}
