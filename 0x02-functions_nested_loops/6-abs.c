8e3e1e654ca83418eb9feab79ea8677f472cf19c
Meine Mac-Obegba <meinemaco@gmail.com>
2026-03-10 00:55:31 +0100

    A function that computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+*Authur: Meine Mac-Obegba
+*Program: WinMingle Community C Training
+*Description: Functions, nested loops - A function that computes the absolute value of an integer.
+.
+*Date: 9th March, 2026
+*/
+
+int _abs(int n){
+    if (n < 0) {
+        return (-n);
+    }
+    else{
+        return (n);
+    }
+
+   	}
+    
+
