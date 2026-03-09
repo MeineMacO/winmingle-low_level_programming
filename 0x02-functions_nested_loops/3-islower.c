f831c8a9adfbfc118dc1054cdf111a03b3f5e37e
Meine Mac-Obegba <meinemaco@gmail.com>
2026-03-09 23:47:32 +0100

    A function that checks for lowercase character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+*Authur: Meine Mac-Obegba
+*Program: WinMingle Community C Training
+*Description: Functions, nested loops - A function that checks for lowercase character.
+*Date: 9th March, 2026
+*/
+ int _islower(int c){
+
+   	if (c >= 'a' && c <= 'z')
+	   return(1);
+	else 
+	   return (0);
+
+   	}
+    
+
