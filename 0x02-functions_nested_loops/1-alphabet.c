b1f873d8ebf2f35bb11ba3a61606b757f96e624e
Meine Mac-Obegba <meinemaco@gmail.com>
2026-03-09 22:25:05 +0100

    A a function that prints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+*Authur: Meine Mac-Obegba
+*Program: WinMingle Community C Training
+*Description: Functions, nested loops - A function that prints the alphabet, in lowercase, followed by a new line.
+*Date: 9th March, 2026
+*/
+void print_alphabet(void){
+    
+    char x = 'a';
+
+	while (x <= 'z'){
+	    _putchar(x);
+		x++;
+	}
+    _putchar('\n');	
+    
+}
+
