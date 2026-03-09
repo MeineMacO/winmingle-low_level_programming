0b931c3904268a28d675d567cdb173f86f2a18de
Meine Mac-Obegba <meinemaco@gmail.com>
2026-03-10 00:05:38 +0100

    A function that checks for alphabetic characters.

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+*Authur: Meine Mac-Obegba
+*Program: WinMingle Community C Training
+*Description: Functions, nested loops - A function that checks for alphabetic characters.
+*Date: 9th March, 2026
+*/
+ int _isalpha(int c){
+
+   	if ((c >= 'A' && c <='Z') || (c >= 'a' && c <= 'z'))
+	   return(1);
+	else 
+	   return (0);
+
+   	}
+    
+
