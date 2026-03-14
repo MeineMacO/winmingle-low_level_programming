8b1bf99eabd5a82713ec84413a1ee6f6eb33ac0e
Meine Mac-Obegba <meinemaco@gmail.com>
2026-03-14 21:32:14 +0100

    A function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/*
+*Authur: Meine Mac-Obegba
+*Program: WinMingle Community C Training
+*Description: Functions, nested loops - A function tthat adds two integers and returns the result.
+*Date: 14th March, 2026
+*/
+
+int add(int x, int y){
+    return(x + y);
+} 
+
