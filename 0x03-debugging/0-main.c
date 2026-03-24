7aed7660ce6b5e21d8a60e3787f36997f8d98d2e
Meine Mac-Obegba <meinemaco@gmail.com>
2026-03-24 16:35:08 +0100

    The main file that tests function that prints if integer is positive or negative.

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+*Authur: Meine Mac-Obegba
+*Department: WinMingle Community Software Engineering Program
+*Project Directory: 0x03-debugging
+*Repository: winmingle-low_level_programming
+*Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+*Description: main - tests function that prints if integer is positive or negative
+* Return: 0
+*Date: 23rd March, 2026
+*/
+int main(void){
+
+    positive_or_negative(0);
+ 
+    return (0);
+}
+
+
