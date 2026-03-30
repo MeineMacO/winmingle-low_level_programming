2bbcae73b7ff5dcb1abbdec0de2a2b6846b5e3bc
Meine Mac-Obegba <meinemaco@gmail.com>
2026-03-30 21:46:23 +0100

    The Header File.

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+*Authur: Meine Mac-Obegba
+*Department: WinMingle Community Software Engineering Program
+*Project Directory: 0x03-debugging
+*Repository: winmingle-low_level_programming
+*Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+*Description: main.h - Header file
+*Date: 23rd March, 2026
+*/
+ 
+void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+ 
+#endif /* MAIN_H */
+
+
+
