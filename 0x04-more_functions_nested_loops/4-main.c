ee8f40b03b58e9bc20c6e9a02c458f886127d102
Meine Mac-Obegba <meinemaco@gmail.com>
2026-04-03 18:28:16 +0100

    The main file that checks the function that prints the numbers, from 0 to 9, except 2 and 4 followed by a new line.

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * Author: Meine Mac-Obegba
+ * Department: WinMingle Community Software Engineering Program
+ * Project Directory: 0x04. C - More functions, more nested loops
+ * Repository: winmingle-low_level_programming
+ * Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+ * Description:  main file - checks the function that prints the numbers, from 0 to 9, except 2 and 4, followed by a new line.
+ * Return: Always 0.
+ * Date: 3rd April, 2026
+ */
+
+int main(void){
+
+    print_most_numbers();
+    return (0);
+}
+
