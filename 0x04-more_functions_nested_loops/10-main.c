b1770f6c562fa85da04f9e057d3a1c41d79e7f17
Meine Mac-Obegba <meinemaco@gmail.com>
2026-04-04 00:52:44 +0100

    The main file that checks the function that draws a triangle.

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * Author: Meine Mac-Obegba
+ * Department: WinMingle Community Software Engineering Program
+ * Project Directory: 0x04. C - More functions, more nested loops
+ * Repository: winmingle-low_level_programming
+ * Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+ * Description:  main file - checks the function that prints a triangle, followed by a new line.
+ * Return: Always 0.
+ * Date: 3rd April, 2026
+ */
+
+int main(void)
+{
+    print_triangle(2);
+    print_triangle(10);
+    print_triangle(1);
+    print_triangle(0);
+    return (0);
+
+}
+
+
