9eade9412f9f6d78a40471056a49b4d28ca92148
Meine Mac-Obegba <meinemaco@gmail.com>
2026-04-03 17:09:44 +0100

    The main file that checks the function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * Author: Meine Mac-Obegba
+ * Department: WinMingle Community Software Engineering Program
+ * Project Directory: 0x04. C - More functions, more nested loops
+ * Repository: winmingle-low_level_programming
+ * Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+ * Description:  main file - checks the function that multiplies two integers.
+ * Return: Always 0.
+ * Date: 3rd April, 2026
+ */
+
+int main(void){
+
+    printf("%d\n", mul(98, 1024));
+    printf("%d\n", mul(-402, 4096));
+
+    return (0);
+}
+
