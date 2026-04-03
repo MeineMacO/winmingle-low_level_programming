d12cfdc97929d16378fbdc75646e8ad66aae5fe4
Meine Mac-Obegba <meinemaco@gmail.com>
2026-04-03 20:28:18 +0100

    The main file that checks the function that prints the numbers, from 0 to 14, 10x followed by a new line.

---
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * Author: Meine Mac-Obegba
+ * Department: WinMingle Community Software Engineering Program
+ * Project Directory: 0x04. C - More functions, more nested loops
+ * Repository: winmingle-low_level_programming
+ * Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+ * Description:  main file - checks the function that prints the numbers, from 0 to 14,10x, followed by a new line.
+ * Return: Always 0.
+ * Date: 3rd April, 2026
+ */
+
+int main(void){
+
+  more_numbers();
+  return (0);
+
+}
+
