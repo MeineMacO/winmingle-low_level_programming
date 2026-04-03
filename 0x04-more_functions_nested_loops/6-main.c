5edcf2aa3918a1c0364096e47ddad58ec6088c7f
Meine Mac-Obegba <meinemaco@gmail.com>
2026-04-03 20:56:36 +0100

    The main file that checks the function that draws a straight line in the terminal.

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ * Author: Meine Mac-Obegba
+ * Department: WinMingle Community Software Engineering Program
+ * Project Directory: 0x04. C - More functions, more nested loops
+ * Repository: winmingle-low_level_programming
+ * Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+ * Description:  main file - checks the function that draws a straight line in the terminal.
+ * Return: Always 0.
+ * Date: 3rd April, 2026
+ */
+
+int main(void){
+
+    print_line(0);
+    print_line(2);
+    print_line(10);
+    print_line(-4);
+    return (0);
+}
+
+
