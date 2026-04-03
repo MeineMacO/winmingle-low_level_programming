f7b5e76e0e647f1c6fb348ec5e2d05c4b4ceb0d6
Meine Mac-Obegba <meinemaco@gmail.com>
2026-04-04 00:25:06 +0100

    The main file that checks the function that draws a square.

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * Author: Meine Mac-Obegba
+ * Department: WinMingle Community Software Engineering Program
+ * Project Directory: 0x04. C - More functions, more nested loops
+ * Repository: winmingle-low_level_programming
+ * Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+ * Description:  main file - checks the function that prints a square, followed by a new line.
+ * Return: Always 0.
+ * Date: 3rd April, 2026
+ */
+
+int main(void)
+{
+    print_square(2);
+    print_square(10);
+    print_square(0);
+    return (0);
+}
+
+
