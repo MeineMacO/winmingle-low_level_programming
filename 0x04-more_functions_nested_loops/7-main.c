7e2a0fca3420633b088f4b47c5516c9946aa56e5
Meine Mac-Obegba <meinemaco@gmail.com>
2026-04-03 21:26:44 +0100

    The main file that checks the function that draws a diagonal line on the terminal.

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * Author: Meine Mac-Obegba
+ * Department: WinMingle Community Software Engineering Program
+ * Project Directory: 0x04. C - More functions, more nested loops
+ * Repository: winmingle-low_level_programming
+ * Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+ * Description:  main file - checks the function that draws a diagonal line on the terminal.
+
+ * Return: Always 0.
+ * Date: 3rd April, 2026
+ */
+
+int main(void)
+{
+    print_diagonal(0);
+    print_diagonal(2);
+    print_diagonal(10);
+    print_diagonal(-4);
+    return (0);
+}
+
+
