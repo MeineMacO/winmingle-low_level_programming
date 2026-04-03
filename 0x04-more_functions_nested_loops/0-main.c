caf86244148c2aee4495f7fa3896d48200bce79b
Meine Mac-Obegba <meinemaco@gmail.com>
2026-04-03 13:34:03 +0100

    The main file that checks 0-isupper.c.

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * Authur: Meine Mac-Obegba
+ * Department: WinMingle Community Software Engineering Program
+ * Project Directory: 0x04. C - More functions, more nested loops
+ * Repository: winmingle-low_level_programming
+ * Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+ * Description:  main file - checks the function that checks for uppercase character.
+ * Return: Always 0.
+ * Date: 3rd April, 2026
+ */
+
+int main(void){
+    char c;
+
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
+
+
