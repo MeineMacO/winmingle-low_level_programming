69959d516540600f779aa4be57ea78e6de71dbc7
Meine Mac-Obegba <meinemaco@gmail.com>
2026-04-03 17:11:48 +0100

    The function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * Author: Meine Mac-Obegba
+ * Department: WinMingle Community Software Engineering Program
+ * Project Directory: 0x04. C - More functions, more nested loops
+ * Repository: winmingle-low_level_programming
+ * Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+ * Description:  2-mul.c - A function that multiplies two integers.
+ * Return: Always 0.
+ * Date: 3rd April, 2026
+ */
+
+int mul(int a, int b)
+{
+    return (a * b);
+
+}
+
