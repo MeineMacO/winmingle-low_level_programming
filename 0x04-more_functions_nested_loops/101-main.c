df2703f23ee65e1ba9b0952098ed3e7e22c54c97
Meine Mac-Obegba <meinemaco@gmail.com>
2026-04-15 13:28:10 +0100

    The main file that checks the function that prints an integer.

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/*
+ * Author: Meine Mac-Obegba
+ * Department: WinMingle Community Software Engineering Program
+ * Project Directory: 0x04. C - More functions, more nested loops
+ * Repository: winmingle-low_level_programming
+ * Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+ * Description:  main file - calls a function that prints an integer.
+ * Return: Always 0.
+ * Date: 3rd April, 2026
+ */
+
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
+}
+
