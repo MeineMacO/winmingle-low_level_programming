bd088779583dddaad49da3486cc79690995ec474
Meine Mac-Obegba <meinemaco@gmail.com>
2026-05-06 18:05:26 +0100

    A function that takes a pointer to an int as a parameter and updates the value it points to 98.

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * Author:  Meine Mac-Obegba
+ * Department:  WinMingle Community Software Engineering Program
+ * Project Directory:  0x04. C - More functions, more nested loops
+ * Repository:  winmingle-low_level_programming
+ * Compilation:  gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+ * Description: 0-reset_to_98.c - A function that takes a pointer to an int as a parameter and updates the value it points to 98.
+ * Date: 1st May, 2026
+ */
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+
+}
