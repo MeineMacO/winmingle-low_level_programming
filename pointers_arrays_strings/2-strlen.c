d8300a40a8b4de8f2a05824008d27135d05b03a6
Meine Mac-Obegba <meinemaco@gmail.com>
2026-05-15 05:09:19 +0100

    A function that returns the length of a string.

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+ * Author:  Meine Mac-Obegba
+ * Department:  WinMingle Community Software Engineering Program
+ * Project Directory:  0x04. C - More functions, more nested loops
+ * Repository:  winmingle-low_level_programming
+ * Compilation:  gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+ * Description: 2-strlen.c - A function that returns the length of a string.
+ * Date: 1st May, 2026
+ */
+
+
+int _strlen(char *s)
+{
+    int length;
+
+    length = 0;
+	while (*s != '\0')
+        {
+                length++;
+                s++;
+        }
+        return (length);
+}
