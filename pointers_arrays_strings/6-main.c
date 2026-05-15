7981e41ca5ba973ee7b6a89cf2c52cb6ceccdd60
Meine Mac-Obegba <meinemaco@gmail.com>
2026-05-15 04:17:30 +0100

    The main file that  calls the function that prints every other character of a string, starting with the first.

---
+++ pointers_arrays_strings/6-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * Author: Meine Mac-Obegba
+ * Department: WinMingle Community Software Engineering Program
+ * Project Directory: 0x04. C -pointers_arrays_strings
+ * Repository: winmingle-low_level_programming
+ * Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+ * Description:  main file - calls the function that prints a every other character of a string, starting with the first.
+ * Return: Always 0.
+ * Date: 1st May, 2026
+ */
+
+
+int main(void)
+{
+    char *str = "0123456789";
+    _puts(str);
+    return (0);
+}
+
