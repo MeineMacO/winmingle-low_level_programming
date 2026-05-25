1a10e3c852ed570c347bdf4a4db952d4f502de22
Meine Mac-Obegba <meinemaco@gmail.com>
2026-05-25 16:04:55 +0100

    The Header file.

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,32 @@
+
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+*Author: Meine Mac-Obegba
+*Department: WinMingle Community Software Engineering Program
+*Project Directory: C - pointers_arrays_strings
+*Repository: winmingle-low_level_programming
+*Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+*Description: main.h - Header file containing function prototypes
+*Date: 1st May, 2026
+*/
+
+int _putchar(char c);
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+
+
+#endif /* MAIN_H */
+
