5d4e02387558b35f759465545fb29c4afc905d50
Meine Mac-Obegba <meinemaco@gmail.com>
2026-05-25 16:09:12 +0100

    The Header file.

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+*Author: Meine Mac-Obegba
+*Department: WinMingle Community Software Engineering Program
+*Project Directory: 0x04. C - More functions, more nested loops
+*Repository: winmingle-low_level_programming
+*Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+*Description: main.h - Header file containing function prototypes
+*Date: 3rd April, 2026
+*/
+
+int _putchar(char c);
+
+int _isupper(int c);
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void fizz_buzz(void);
+void print_triangle(int size);
+void prime_factor(long n);
+void print_number(int n);
+
+#endif /* MAIN_H */
