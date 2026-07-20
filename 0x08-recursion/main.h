9ed76ad22b32f2c30cd18bffe55318275f37ae14
Meine Mac-Obegba <meinemaco@gmail.com>
2026-07-20 01:40:11 +0100

    The Header File.

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/*
+* Author:  Meine Mac-Obegba
+* Department:  WinMingle Community Software Engineering Program
+* Project Directory:  0x08-recursion
+* Repository:  winmingle-low_level_programming
+* Compilation:  gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+* Description: main.h - Header file containing function prototypes
+* Date: 1st June, 2026
+*/
+
+int _putchar(char c);
+
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+
+#endif /* MAIN_H */
+
+
