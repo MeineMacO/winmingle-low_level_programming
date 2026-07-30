e8cee290b9f80e49d12aae9fb8a12a531d0f520e
Meine Mac-Obegba <meinemaco@gmail.com>
2026-07-30 14:32:15 +0100

    The main file that calls the 0-puts_recursion.c function that  prints a string, followed by a new line.

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * Author: Meine Mac-Obegba
+ * Department: WinMingle Community Software Engineering Program
+ * Project Directory: 0x08-recursion
+ * Repository: winmingle-low_level_programming
+ * Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+ * Description:  main file - calls the 0-puts_recursion.c funtion that prints a string, followed by a new line.
+* Date: 1st June, 2026
+*/
+
+
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
+
