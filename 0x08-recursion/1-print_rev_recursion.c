8aa74256bdcd50841a50cc2af95288c1bac5a4d1
Meine Mac-Obegba <meinemaco@gmail.com>
2026-07-30 14:50:52 +0100

    The 1-print_rev_recursion.c function that prints a string in reverse.

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * Author: Meine Mac-Obegba
+ * Department: WinMingle Community Software Engineering Program
+ * Project Directory: 0x08-recursion
+ * Repository: winmingle-low_level_programming
+ * Compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+ * Description:  The 1-print_rev_recursion.c funtion that prints a string in reverse.
+* Date: 1st June, 2026
+*/
+
+
+void _print_rev_recursion(char *s)
+{
+    if (*s == '\0')
+	    return;
+    _print_rev_recursion(s + 1);
+    _putchar(*s);                 
+}
+
